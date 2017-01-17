2e3b48e55d4c128404284da492d22e2454390942
audiosprite <jpfiorilla@gmail.com>
2017-01-17 16:40:53 -0500

    finds superwords

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const Twit = require('twit');
+// const credientials = {
+
+// };
+// const T = new Twit(credientials);
 
 require.extensions['.txt'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
@@ -10,4 +14,32 @@ const dictionary = words.split("\n");
 Array.prototype.pick = function() {
     return this[Math.floor(Math.random()*this.length)];
 };
+const pickWord = function(){
+    let picked = '';
+    while (picked.length > 5 || picked.length < 3){
+        picked = dictionary.pick();
+    }
+    return picked;
+}
 
+String.prototype.findSuperWords = function(){
+    let superwords = [];
+    for (var i = 0; i < dictionary.length; i++){
+        if (dictionary[i].includes(this) && dictionary[i].length !== this.length) superwords.push(dictionary[i]);
+    }
+    // 140-28 = 112
+    return superwords;
+}
+
+const findVars = function(){
+    let superWords = [];
+    while (!superWords.length){
+        var word = pickWord();
+        superWords = word.findSuperWords();
+    }
+    console.log(word, '\n', superWords.join(' '));
+}
+findVars();
+
+const meow = 'meow';
+let status = `List of words containing "${meow}": ` + '.';
